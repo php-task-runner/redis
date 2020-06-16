3b7cbf0a77757bf18929096529e535af26fbcad3
Claudiu Cristea <clau.cristea@gmail.com>
2020-06-16 19:00:52 +0300

    Fix coding standards.

--- src/TaskRunner/Commands/RedisCommands.php
+++ src/TaskRunner/Commands/RedisCommands.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace TaskRunner\Redis\TaskRunner\Commands;
 
@@ -10,21 +10,21 @@ use Predis\Client;
 /**
  * Provides commands for Redis cache backend.
  */
-class RedisCommands extends AbstractCommands {
-
-  /**
-   * Flushes the Redis backend.
-   *
-   * @command redis:flush-all
-   */
-  public function flushAll(): void {
-    $parameters = array_filter([
-      'host' => getenv('REDIS_HOST'),
-      'port' => getenv('REDIS_PORT'),
-      'password' => getenv('REDIS_PASSWORD'),
-    ]);
-    (new Client($parameters))->flushall();
-    $this->say('Redis backend flushed');
-  }
-
+class RedisCommands extends AbstractCommands
+{
+    /**
+     * Flushes the Redis backend.
+     *
+     * @command redis:flush-all
+     */
+    public function flushAll(): void
+    {
+        $parameters = array_filter([
+            'host' => getenv('REDIS_HOST'),
+            'port' => getenv('REDIS_PORT'),
+            'password' => getenv('REDIS_PASSWORD'),
+        ]);
+        (new Client($parameters))->flushall();
+        $this->say('Redis backend flushed');
+    }
 }
