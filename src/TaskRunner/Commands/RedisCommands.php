0fc8f6f285bfdc618b2b7bcf54e14b14d026a0d3
Claudiu Cristea <clau.cristea@gmail.com>
2022-06-02 14:24:34 +0300

    Make predis/predis optional.

--- src/TaskRunner/Commands/RedisCommands.php
+++ src/TaskRunner/Commands/RedisCommands.php
@@ -19,12 +19,35 @@ class RedisCommands extends AbstractCommands
      */
     public function flushAll(): void
     {
-        $parameters = array_filter([
-            'host' => getenv('REDIS_HOST'),
-            'port' => getenv('REDIS_PORT'),
-            'password' => getenv('REDIS_PASSWORD'),
-        ]);
-        (new Client($parameters))->flushall();
+        if ($this->useRedisExtension()) {
+            $client = new \Redis();
+            $port = getenv('REDIS_PORT') ? (int) getenv('REDIS_PORT') : 6379;
+            $client->connect(getenv('REDIS_HOST'), $port);
+            if (!empty(getenv('REDIS_PASSWORD'))) {
+                $client->auth(getenv('REDIS_PASSWORD'));
+            }
+        }
+        elseif ($this->usePredisLibrary()) {
+            $parameters = array_filter([
+                'host' => getenv('REDIS_HOST'),
+                'port' => getenv('REDIS_PORT'),
+                'password' => getenv('REDIS_PASSWORD'),
+            ]);
+            $client = new Client($parameters);
+        } else {
+            throw new \Exception('Either PHP Redis extension or predis/predis should be inetslled.');
+        }
+        $client->flushall();
         $this->say('Redis backend flushed');
+    }
+
+    private function useRedisExtension(): bool
+    {
+        return class_exists(\Redis::class);
+    }
+
+    private function usePredisLibrary(): bool
+    {
+        return class_exists(Client::class);
     }
 }
